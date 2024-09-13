00e33ce7819a5988be9d6ac2d31c8972761b202d
oscar <oscar.moreno@cti.espol.edu.ec>
2024-09-13 13:39:22 -0500

    Type with valibot

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { z } from 'zod'
+import { object, string, number, InferOutput, parse } from 'valibot'
 import { SearchType } from '../types'
 
 
@@ -17,16 +18,26 @@ import { SearchType } from '../types'
 //}
 
 //Zod
-const Weather = z.object({
-  name: z.string(),
-  main: z.object({
-    temp: z.number(),
-    temp_max: z.number(),
-    temp_min: z.number(),
+//const Weather = z.object({
+//  name: z.string(),
+//  main: z.object({
+//    temp: z.number(),
+//    temp_max: z.number(),
+//    temp_min: z.number(),
+//  })
+//})
+//type Weather = z.infer<typeof Weather>
+
+//Valibot
+const WeatherSchema = object({
+  name: string(),
+  main: object({
+    temp: number(),
+    temp_max: number(),
+    temp_min: number()
   })
 })
-
-type Weather = z.infer<typeof Weather>
+type Weather = InferOutput<typeof WeatherSchema>
 
 export default function useWeather() {
 
@@ -55,16 +66,25 @@ export default function useWeather() {
       //}
 
       //Zod
+      //const { data: weatherResult } = await axios(weatherUrl)
+      //const result = Weather.safeParse(weatherResult)
+      //console.log(result)
+      //if (result.success) {
+      //  console.log(result.data.name)
+      //  console.log(result.data.main.temp)
+      //} else {
+      //  console.log('respuesta mal formada')
+      //}
+
+      //Valibot
       const { data: weatherResult } = await axios(weatherUrl)
-      const result = Weather.safeParse(weatherResult)
-      console.log(result)
-      if (result.success) {
-        console.log(result.data.name)
-        console.log(result.data.main.temp)
+      const result = parse(WeatherSchema, weatherResult)
+      if (result) {
+        console.log(result.name)
+        console.log(result.main.temp)
       } else {
         console.log('respuesta mal formada')
       }
-
 
     } catch (error) {
       console.log(error)
