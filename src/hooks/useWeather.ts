21295a2f64f1f799b983ad2623ee2c2694838b45
oscar <oscar.moreno@cti.espol.edu.ec>
2024-09-13 12:08:02 -0500

    create api calls

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,16 +1,23 @@
 import axios from 'axios'
-import { SearchType } from '../types'
+import { SearchType, Weather } from '../types'
 
-export default function useWeather (){
-  
-  const fetchWeather = async(search : SearchType) => {
-    const appid = 'dca4ddf188c7529501334a6fd665bdc0'
+export default function useWeather() {
+
+  const fetchWeather = async (search: SearchType) => {
+    const appid = import.meta.env.VITE_API_KEY
     try {
-       const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appid}`
+      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appid}`
+      const { data } = await axios(geoUrl)
 
-       const data = await axios(geoUrl)
-       console.log(data)
-       console.log(geoUrl)
+      const lat = data[0].lat
+      const lon = data[0].lon
+
+      //castear el type
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}`
+      const { data: weatherResult } = await axios<Weather>(weatherUrl)
+      console.log(weatherResult.main.temp)
+      console.log(weatherResult.main.temp_max)
+
     } catch (error) {
       console.log(error)
     }
