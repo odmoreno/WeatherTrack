5b1d448f71ab5cbc7591521533859e01efc427b6
Oscar Moreno <oscar.moreno@espol.edu.ec>
2024-09-13 23:26:10 -0500

    add weather detail component

--- src/components/WeatherDetail/WeatherDetail.tsx
+++ src/components/WeatherDetail/WeatherDetail.tsx
@@ -1,8 +1,20 @@
+import { Weather } from "../../hooks/useWeather"
+import { formatTemperature } from "../../utils"
+import style from './WeatherDetail.module.css'
 
-export default function WeatherDetail() {
+type WeatherDetailProps = {
+    weather : Weather
+}
+
+export default function WeatherDetail({weather}: WeatherDetailProps) {
     return (
-        <div>
-            WeatherDetail
+        <div className={style.container}>
+            <h2>Clima de: {weather.name}</h2>
+            <p className={style.current}>{formatTemperature(weather.main.temp)}&deg;C</p>
+            <div className={style.temperatures}>
+                <p>Min: <span>{formatTemperature(weather.main.temp_min)}&deg;C</span></p>
+                <p>Max: <span>{formatTemperature(weather.main.temp_max)}&deg;C</span></p>
+            </div>
         </div>
     )
 }
